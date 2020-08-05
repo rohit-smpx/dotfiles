0ce721948dda5cb3ac58232aa5cd10c615e3e6ed
Rohit Gohri <work@rohit.page>
2020-08-05 23:20:11 +0530

    update aliases

--- common/config/zsh/base/aliases.zsh
+++ common/config/zsh/base/aliases.zsh
@@ -1,3 +1,8 @@
 alias zshconfig="code ~/.zshrc"
 alias zshcustom="code ~/.config/zsh/custom.zsh"
+alias zshaliases="code ~/.config/zsh/base/aliases.zsh"
 alias zshreload="source ~/.zshrc"
+
+alias gbltd="gb -vv | grep origin | grep gone | awk '{print \$1}';"
+alias gbld="gb -vv | grep origin | grep gone | awk '{print \$1}' | xargs -L 1 git branch -d"
+alias grpo="gr prune origin"
