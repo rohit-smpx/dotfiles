fce90cf384fe6507f697ffaf277df968d7815a13
Rohit Gohri <work@rohit.page>
2020-12-31 16:39:00 +0530

    add alias

--- common/config/zsh/base/aliases.zsh
+++ common/config/zsh/base/aliases.zsh
@@ -6,3 +6,5 @@ alias zshreload="source ~/.zshrc"
 alias gbltd="gb -vv | grep origin | grep gone | awk '{print \$1}';"
 alias gbld="gb -vv | grep origin | grep gone | awk '{print \$1}' | xargs -L 1 git branch -d"
 alias grpo="gr prune origin"
+
+alias dupwd="du -h -d 1 ./"
