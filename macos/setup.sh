adcbd4aa16d0dca7fd94fae273b28f556f8d9e48
Rohit Gohri <work@rohit.page>
2021-02-10 17:20:06 +0530

    fix setup

--- macos/setup.sh
+++ macos/setup.sh
@@ -3,5 +3,6 @@ set -e
 ../common/setup.sh
 CURRENT_DIR=$(cd $(dirname "$0") && pwd)
 
+mkdir -p ~/.config/zsh/custom/
 ln -sv $CURRENT_DIR/config/zsh/macos.zsh ~/.config/zsh/custom/macos.zsh
 ln -sv $CURRENT_DIR/config/zsh/iTerm2-ssh.zsh ~/.config/zsh/custom/iTerm2-ssh.zsh
