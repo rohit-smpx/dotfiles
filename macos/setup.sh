498d23c599ffffe20818de58c1e2120b6567706e
Rohit Gohri <work@rohit.page>
2021-02-11 15:07:23 +0530

    add mac config overrides

--- macos/setup.sh
+++ macos/setup.sh
@@ -6,3 +6,13 @@ CURRENT_DIR=$(cd $(dirname "$0") && pwd)
 mkdir -p ~/.config/zsh/custom/
 ln -sv $CURRENT_DIR/config/zsh/macos.zsh ~/.config/zsh/custom/macos.zsh
 ln -sv $CURRENT_DIR/config/zsh/iTerm2-ssh.zsh ~/.config/zsh/custom/iTerm2-ssh.zsh
+
+# Mac Specific Overrides
+
+# Show hidden files
+defaults write com.apple.Finder AppleShowAllFiles true
+killall Finder
+
+# Disable spaces rearranging
+# SEE: https://www.defaults-write.com/disable-automatically-rearrange-spaces-based-on-recent-use/
+defaults write com.apple.dock mru-spaces -bool false
